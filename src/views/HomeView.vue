b778a3e97128d95f5e24e7a840e218ddbf3a607b
LuisFelipe1406 <luisfelipestututi@gmail.com>
2024-08-06 22:53:32 -0300

    feat: initial commit; test version with static image

---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div id="divMain">
+
+  </div>
+</template>
+
+<style scoped>
+  #divMain {
+    background: url('/img/main.png') no-repeat center center fixed;
+    background-size: cover;
+
+    height: 100vh;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
