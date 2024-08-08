74fe66d91ad63a6a9c70c89fb19e138760e43ac0
LuisFelipe1406 <luisfelipestututi@gmail.com>
2024-08-07 22:53:37 -0300

    feat: update dist folder

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
+
 </script>
 
 <template>
-  <div id="divMain">
-
-  </div>
+  <div id="divMain"></div>
 </template>
 
 <style scoped>
   #divMain {
-    background: url('/img/main.png') no-repeat center center fixed;
+    background-image: url('/img/main.png');
     background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
 
     height: 100vh;
 
