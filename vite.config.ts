e8a0a711ed8784d0523b95a5bdd5bf3007c2ab3f
LuisFelipe1406 <luisfelipestututi@gmail.com>
2024-08-07 23:15:26 -0300

    fix: adjust vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/borbaconsultoria/', // Ajuste este caminho conforme necessário
   plugins: [
     vue(),
     vueJsx(),
