3ca4b1d9d45895d00d8b9887bdbd5132a3680113
LuisFelipe1406 <luisfelipestututi@gmail.com>
2024-08-07 21:00:31 -0300

    fix: att vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,1 @@
-module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "borbaconsultoria" : "/",
-  };
+export const publicPath = process.env.NODE_ENV === "production" ? "borbaconsultoria" : "/";
