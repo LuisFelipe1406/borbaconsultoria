21d2e8928711128d43f301582087f9922f4a1e75
LuisFelipe1406 <luisfelipestututi@gmail.com>
2024-08-08 22:09:30 -0300

    feat: add mobile version; change CNAME folder

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,4 +21,12 @@
     justify-content: center;
     align-items: center;
   }
+
+  /* Alteramos o espaçamento dos elementos de acordo com o tamanho da tela */
+  @media only screen and (max-width: 1200px) {
+    #divMain {
+      background-image: url('/img/main-mobile.png');
+    }
+  }
+
 </style>
